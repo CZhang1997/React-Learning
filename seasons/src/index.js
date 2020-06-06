b4d0cb3fcbb4935052bd6f7d7918fab6909cbbc0
Churong Zhang <cxz173430@utdallas.edu>
2020-06-06 12:21:31 -0500

    finish class components

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,27 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
-
-
+import Spinner from './Spinner';
 
 class App extends React.Component {
     constructor(props) {
         super(props);
         // this is the only time do direct assignment, is when you create it
-        this.state = { lat: null, errorMessage: '' };
+        //this.state = { lat: null, errorMessage: '' };
+
+    }
+    state = { lat: null, errorMessage: '' };
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                // change the state must use setState
-                this.setState({ lat : position.coords.latitude});
-            },
-            error => {
-                console.log(error.message);
-                this.setState( {errorMessage : error.message});
-            }
+            position => this.setState({ lat : position.coords.latitude}),
+            error => this.setState( {errorMessage : error.message})
         );
     }
-    // must define
-    render () {
+    renderContent() {
         if(this.state.errorMessage && !this.state.lat)
         {
             return <div>
@@ -31,11 +27,13 @@ class App extends React.Component {
         
         if(!this.state.errorMessage && this.state.lat)
         {
-            return <div>
-                Latitude: {this.state.lat}
-                </div>
+            return <SeasonDisplay lat={this.state.lat}/>
         }
-        return <div>Loading!</div>
+        return <Spinner text="Please allow the loaction request"/>;
+    }
+    // must define
+    render () {
+    return <div className="border red">{this.renderContent()}</div>;
     }
 }
 ReactDOM.render(<App />, document.querySelector("#root"));
