e2d15239ae72ec40c70c2d93b5f5a8c165887f46
Churong Zhang <cxz173430@utdallas.edu>
2020-06-07 15:38:29 -0500

    finish picture search

--- pics/src/components/ImageCard.js
+++ pics/src/components/ImageCard.js
@@ -4,18 +4,23 @@ class ImageCard extends React.Component {
 
     constructor(props) {
         super(props);
-
+        this.state = {spans: 0};
         this.imageRef = React.createRef();
     }
 
     componentDidMount () {
-        console.log(this.imageRef.current);
+        this.imageRef.current.addEventListener('load', this.setSpans);
         // console.log(this.imageRef.current.clientHeight);
+    }
+    setSpans = () => {
+        const height = this.imageRef.current.clientHeight;
+        const spans = Math.ceil(height/10) + 1;
+        this.setState({ spans });
     }
     render()
     {
         const {description, urls} = this.props.image;
-        return (<div>
+        return (<div style={{gridRowEnd:`span ${this.state.spans}`}}>
             <img 
                 ref={this.imageRef}
                 alt={description}
